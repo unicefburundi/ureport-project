f60faebbf31c7ad491024b82923ee30f46d0e76c
Alfred Assey M <asseym@gmail.com>
2013-02-16 00:04:33 +0300

    moved kannel backends to main settings

--- ureport_project/settings.py
+++ ureport_project/settings.py
@@ -247,9 +247,44 @@ INSTALLED_APPS = (
 
 INSTALLED_BACKENDS = {
     "message_tester": {
-        "ENGINE": "rapidsms.contrib.httptester.backend",
+        "ENGINE": "rapidsms.backends.bucket",
+    },
+    # other backends, if any
+    "kannel-fake-smsc" : {
+        "ENGINE":  "rapidsms.backends.kannel",
+        "sendsms_url": "http://127.0.0.1:13013/cgi-bin/sendsms",
+        "sendsms_params": {"smsc": "FAKE",
+                           "from": "155", # not set automatically by SMSC
+                           "username": "rapidsms",
+                           }, # KANNEL_PASSWORD set in localsettings.py
+        "coding": 0,
+        "charset": "ascii",
+        "encode_errors": "ignore", # strip out unknown (unicode) characters
+    },
+    "smsbu" : {
+        "ENGINE":  "rapidsms.backends.kannel",
+        "sendsms_url": "http://gateway.smbu.bi/sendsms",
+        "sendsms_params": {"smsc": "smbu",
+                           "user": "unicef",
+                           "password": "burundi155",
+                           "sender": "155",
+                           "msisdn": "",
+                           "from": "155", # not set automatically by SMSC
+                           "username": "rapidsms",
+                           }, # KANNEL_PASSWORD set in localsettings.py
+        "coding": 0,
+        "charset": "ascii",
+        "encode_errors": "ignore", # strip out unknown (unicode) characters
     },
 }
+
+SMS_APPS = [
+    "echo",
+    "ureport",
+    "script",
+    "poll",
+    "unregister",
+]
 
 # this rapidsms-specific setting defines which views are linked by the
 # tabbed navigation. when adding an app to INSTALLED_APPS, you may wish
