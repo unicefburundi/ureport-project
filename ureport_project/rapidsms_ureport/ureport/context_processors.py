e072a607d3833893121eb740d048c4fbd405b313
mbanje <mbanjejvernack@gmail.com>
2014-07-13 17:48:00 +0200

    Make the context processor "voices" count the number of urepoters within the group of a connected admin(not superuser)

--- ureport_project/rapidsms_ureport/ureport/context_processors.py
+++ ureport_project/rapidsms_ureport/ureport/context_processors.py
@@ -31,12 +31,27 @@ def voices(request):
     except QuoteBox.DoesNotExist:
         quote = None
 
+    user = request.user
+
+    #The variable below will contain the numbers of ureporters in the group 
+    #the user who is connected belongs to. It will contain a valid value if
+    #that user has loged in the system and if it is not a superuser.
+    total_ureporters_for_one_group = 0
+
+
+    if user.groups.count()>0 and not user.is_superuser:
+        id_of_the_group_of_this_user = user.groups.get_query_set()[0].id
+        total_ureporters_for_one_group = Contact.objects.filter(groups__id=id_of_the_group_of_this_user).exclude(
+            connection__identity__in=Blacklist.objects.values_list('connection__identity', flat=True)).count()
+
+    
     context = {
         'total_ureporters': Contact.objects.exclude(
             connection__identity__in=Blacklist.objects.values_list('connection__identity', flat=True)).count(),
         'polls': Poll.objects.exclude(contacts=None, start_date=None).exclude(pk__in=[297, 296, 349, 350]).order_by(
                 '-start_date'),
         'deployment_id': getattr(settings, 'DEPLOYMENT_ID', 1),
+        'total_ureporters_for_one_group':total_ureporters_for_one_group,
         'quote': quote,
         'geoserver_url': getattr(settings, 'GEOSERVER_URL', None),
         'map_bounds': getattr(settings, 'OPEN_LAYERS_MAP_BOUNDS', '3226872.59281459, -496397.855648315, 3434638.20264732, -256041.778361828'),
